b961ef06e2da76886f03ccd000d06e14ae2224b1
Atharva-kul <atharvakulkarni2112005@gmail.com>
2026-01-23 08:41:14 +0530

    Refactor player variable and improve comments

--- prototype/player/staates/player_states.gd
+++ prototype/player/staates/player_states.gd
@@ -1,24 +1,37 @@
 class_name PlayerState extends Node
 
-var player : player
+var player_node : player
 var next_state : PlayerState = null
 
 #region /// state reference
-# reference to alll other statwes
+# reference to all other states
 #endregion
 
-func _init() -> void:
+func init() -> void:
+	#print("init ", name)
 	pass
 	
 func enter() -> void:
+	#print("enter ", name)
 	pass
 	
 func exit() -> void:
+	#print("exit ", name)
 	pass
 	
 	
 func handle_input(_event : InputEvent) -> PlayerState:
+	
+	return
+		
+	#print("Input ", name)
+	return next_state
+	
+func process(_delta: float) -> PlayerState:
+	#print("P ", name, _delta)
+	player_node.position.x += 1
 	return next_state
 		
 func physics_process(_delta: float) -> PlayerState:
+	#print("Plysics works")
 	return next_state
